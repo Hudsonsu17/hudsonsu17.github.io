275b0562fd984f49f38e27f722e915e03e4bf658
Use your GitHub username <Enter your email address>
2019-02-15 19:50:14 -0700

    updated portal page

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,13 +1,13 @@
 //This is to get the current date to put into the footer of the Town Page//
-var today = new Date();
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-//let year = today.getFullYear();
-var month = months[today.getMonth()];
+let today = new Date();
+//let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let year = today.getFullYear();
+//let month = months[today.getMonth()];
 //let day = today.getDay();
 
 
 //let date = today + ", " + day + month + year;
 
-document.getElementById("currentdate").innerhtml = month;
+document.getElementById('currentdate').innerhtml = year;
 
 //Wednesday, 13 Febraury 2019//
