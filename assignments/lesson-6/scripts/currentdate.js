1a2cb80b521a741228bd0e00b3656c94cebc2f15
Use your GitHub username <Enter your email address>
2019-02-15 22:31:35 -0700

    currentdate javascript working

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,13 +1,15 @@
 //This is to get the current date to put into the footer of the Town Page//
 let today = new Date();
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 let year = today.getFullYear();
 let month = months[today.getMonth()];
+let week = weekday[today.getDay()];
 let day = today.getDay();
 
 
-let date = today + ", " + day + month + year;
+let date = week + ", " + day + " " + month + " " + year;
 
-document.getElementById('currentdate').innerHTML = year;
+document.getElementById('currentdate').innerHTML = date;
 
 //Wednesday, 13 Febraury 2019//
