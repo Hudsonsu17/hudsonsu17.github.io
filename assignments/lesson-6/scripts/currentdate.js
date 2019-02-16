3f384d582c74feb89271766f370c5e5df343d533
Use your GitHub username <Enter your email address>
2019-02-15 22:21:19 -0700

    fixed responsive menu

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,13 +1,13 @@
 //This is to get the current date to put into the footer of the Town Page//
 let today = new Date();
-//let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let year = today.getFullYear();
-//let month = months[today.getMonth()];
-//let day = today.getDay();
+let month = months[today.getMonth()];
+let day = today.getDay();
 
 
-//let date = today + ", " + day + month + year;
+let date = today + ", " + day + month + year;
 
-document.getElementById('currentdate').innerhtml = year;
+document.getElementById('currentdate').innerHTML = year;
 
 //Wednesday, 13 Febraury 2019//
--- assignments/lesson-6/scripts/hamburger.js
+++ assignments/lesson-6/scripts/hamburger.js
@@ -1,3 +1,12 @@
 function toggleMenu() {
     document.getElementsByClassName("menu")[0].classList.toggle("responsive");
 }
+
+function myFunction() {
+    var x = document.getElementById("navmenu");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
