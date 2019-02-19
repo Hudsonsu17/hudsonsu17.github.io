1b92b703c045c4818517f12ed8ed147a8ddd048f
Use your GitHub username <Enter your email address>
2019-02-19 16:57:14 -0700

    windchill javascript works

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -3,5 +3,6 @@ let speed = parseInt(document.getElementById('speed').innerHTML);
 
 let windSpeed = Math.pow(speed, 0.16);
 let result = 35.74 + (0.6215 * temp) - (35.75 * windSpeed) + 0.4275 * temp * windSpeed;
+let x = Math.round(result);
 
-document.getElementById('output').innerHTML = "Wind Chill: " + result + " &deg;F";
+document.getElementById('output').innerHTML = "Wind Chill: " + x + " &deg;F";
