3e254a0e735b5c0ee8b8c9d3136f4d93a9c0b5ff
Use your GitHub username <Enter your email address>
2019-02-26 11:46:25 -0700

    created lesson 8 folder

---
+++ assignments/lesson-8/scripts/windchill.js
@@ -0,0 +1,8 @@
+let temp = parseInt(document.getElementById('temp').innerHTML);
+let speed = parseInt(document.getElementById('speed').innerHTML);
+
+let windSpeed = Math.pow(speed, 0.16);
+let result = 35.74 + (0.6215 * temp) - (35.75 * windSpeed) + 0.4275 * temp * windSpeed;
+let x = Math.round(result);
+
+document.getElementById('output').innerHTML = "Wind Chill: " + x + " &deg;F";
