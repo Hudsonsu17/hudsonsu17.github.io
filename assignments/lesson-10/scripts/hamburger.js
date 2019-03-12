7c78038d55e843ced284264e787b404ec2e7e6e5
Use your GitHub username <Enter your email address>
2019-03-12 10:45:00 -0600

    updated hamburger java

--- assignments/lesson-10/scripts/hamburger.js
+++ assignments/lesson-10/scripts/hamburger.js
@@ -1,11 +1,3 @@
 function toggleMenu() {
     document.getElementsByClassName("menu")[0].classList.toggle("responsive");
-    
-}function myFunction() {
-  var x = document.getElementById("navmenu");
-  if (x.className === "menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "menu";
-    }
    }
