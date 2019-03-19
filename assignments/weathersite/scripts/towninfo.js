c3336493bfcd1f443c29136352b50679010efb50
Use your GitHub username <Enter your email address>
2019-03-19 15:02:55 -0600

    updates to pages and images

--- assignments/weathersite/scripts/towninfo.js
+++ assignments/weathersite/scripts/towninfo.js
@@ -40,36 +40,6 @@ request.onload = function () {
             output.appendChild(para);
         }
     });
-    // This is for the Fish Haven section
-    towns.forEach(town => {
-        if (town.name == 'Fish Haven') {
-            let para = document.createElement('p');
-            let homeH3 = document.createElement('h3');
-            let motto = document.createElement('div');
-            motto.setAttribute("class", "jsonmotto");
-            let year = document.createElement('div');
-            year.setAttribute("class", "notes1");
-            let population = document.createElement('div');
-            population.setAttribute("class", "notes1");
-            let rainfall = document.createElement('div');
-            rainfall.setAttribute("class", "notes1");
-            let x = document.createElement("img");
-            x.setAttribute("src", "images/fishhaven.jpg");
-            x.setAttribute("alt", "Fish Haven Town");
-            homeH3.textContent = town.name;
-            motto.textContent = town.motto;
-            year.textContent = 'Year Founded: ' + town.yearFounded;
-            population.textContent = 'Population: ' + town.currentPopulation;
-            rainfall.textContent = 'Annual Rain Fall: ' + town.averageRainfall;
-            para.appendChild(homeH3);
-            para.appendChild(motto);
-            para.appendChild(year);
-            para.appendChild(population);
-            para.appendChild(rainfall);
-            para.appendChild(x);
-            output.appendChild(para);
-        }
-    });
     // This is for the Soda Springs section
     towns.forEach(town => {
         if (town.name == 'Soda Springs') {
@@ -86,6 +56,36 @@ request.onload = function () {
             let x = document.createElement("img");
             x.setAttribute("src", "images/sodasprings.jpg");
             x.setAttribute("alt", "Soda Springs Town");
+            homeH3.textContent = town.name;
+            motto.textContent = town.motto;
+            year.textContent = 'Year Founded: ' + town.yearFounded;
+            population.textContent = 'Population: ' + town.currentPopulation;
+            rainfall.textContent = 'Annual Rain Fall: ' + town.averageRainfall;
+            para.appendChild(homeH3);
+            para.appendChild(motto);
+            para.appendChild(year);
+            para.appendChild(population);
+            para.appendChild(rainfall);
+            para.appendChild(x);
+            output.appendChild(para);
+        }
+    });
+    // This is for the Fish Haven section
+    towns.forEach(town => {
+        if (town.name == 'Fish Haven') {
+            let para = document.createElement('p');
+            let homeH3 = document.createElement('h3');
+            let motto = document.createElement('div');
+            motto.setAttribute("class", "jsonmotto");
+            let year = document.createElement('div');
+            year.setAttribute("class", "notes1");
+            let population = document.createElement('div');
+            population.setAttribute("class", "notes1");
+            let rainfall = document.createElement('div');
+            rainfall.setAttribute("class", "notes1");
+            let x = document.createElement("img");
+            x.setAttribute("src", "images/fishhaven.jpg");
+            x.setAttribute("alt", "Fish Haven Town");
             homeH3.textContent = town.name;
             motto.textContent = town.motto;
             year.textContent = 'Year Founded: ' + town.yearFounded;
