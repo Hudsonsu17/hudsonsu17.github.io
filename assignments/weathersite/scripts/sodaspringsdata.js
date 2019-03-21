c43a70c2ba9b4797e92dddf5e8c37059b3f62238
Use your GitHub username <Enter your email address>
2019-03-20 18:01:29 -0600

    updates to final folder and files names

--- assignments/weathersite/scripts/sodaspringsdata.js
+++ assignments/weathersite/scripts/sodaspringsdata.js
@@ -1,3 +1,26 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&APPID=98d88dd5f6ce4902ff35e6383cb270c3';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+
+    document.getElementById('sky').innerHTML = "Currently: " + weatherData.weather[0].main;
+    document.getElementById('currenttemp').innerHTML = "High Temp: " + weatherData.main.temp_max + " &deg;F";
+    document.getElementById('humid').innerHTML = "Humidity: " + weatherData.main.humidity + "%";
+    document.getElementById('speed').innerHTML = "Wind Speed: " + weatherData.wind.speed + " mph";
+
+    let temp = weatherData.main.temp_max;
+    let speed = weatherData.wind.speed;
+
+    let windSpeed = Math.pow(speed, 0.16);
+    let result = 35.74 + (0.6215 * temp) - (35.75 * windSpeed) + 0.4275 * temp * windSpeed;
+    let x = Math.round(result);
+
+    document.getElementById('output').innerHTML = "Wind Chill: " + x + " &deg;F";
+}
+
 let forecastRequest = new XMLHttpRequest();
 let apiURLline = 'https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&APPID=98d88dd5f6ce4902ff35e6383cb270c3';
 forecastRequest.open('Get', apiURLline, true);
@@ -68,5 +91,37 @@ forecastRequest.onload = function () {
         let desc = y;
         document.getElementById('foreimg' + i).setAttribute('src', icon);
         document.getElementById('foreimg' + i).setAttribute('alt', desc);
+    }
+}
+
+let events = document.querySelector('#events');
+
+let requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    let getData = request.response;
+    let towns = getData['towns'];
+
+    for (let i = 0; i < towns.length; i++) {
+        if (towns[i].name == "Soda Springs") {
+            let prestonEvents = towns[i].events;
+            let eventName = 0; 
+
+            for (let j = 0; j < prestonEvents.length; j++) {
+                let event = prestonEvents[eventName];
+                eventName++;
+
+            let eventSection = document.createElement('article');
+            let h4List = document.createElement('h4');
+            h4List.textContent = event;
+            eventSection.appendChild(h4List);
+            events.appendChild(eventSection);
+  
+            }
+        }
     }
 }
