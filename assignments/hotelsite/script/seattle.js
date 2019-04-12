8dfa33a5b9d567968c5afefc096922bbc6f8993d
Use your GitHub username <Enter your email address>
2019-04-12 13:23:33 -0600

    updated website

--- assignments/hotelsite/script/seattle.js
+++ assignments/hotelsite/script/seattle.js
@@ -1,15 +1,3 @@
-let seattleRequest = new XMLHttpRequest();
-let seattleWeather = 'https://api.openweathermap.org/data/2.5/weather?id=5809844&units=imperial&APPID=98d88dd5f6ce4902ff35e6383cb270c3';
-seattleRequest.open('Get', seattleWeather, true);
-seattleRequest.send();
-
-seattleRequest.onload =  function () {
-    let weatherData = JSON.parse(seattleRequest.responseText);
-
-    document.getElementById('seattle').innerHTML = "CURRENT TEMPERATURE: " + weatherData.main.temp + " &deg;F";
-    document.getElementById('forecast3').innerHTML = "WEATHER CONDITION: " + weatherData.weather[0].description;
-}
-
 //This is to get the closed dates for the temple
 
 let events = document.querySelector('article');
